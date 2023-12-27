e8dd4f3cc2a48769432dfb277f0fe6c0d538bcd2
CharlotteLouise <150289744+Macklou@users.noreply.github.com>
2023-12-27 16:37:39 +0000

    update index.py

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
+import tkinter as tk
+from tkinter import BooleanVar
 import random
 import string
 
-def generate_password(length, use_numbers=True, use_lowercase=True, use_uppercase=True, use_symbols=True, use_spaces=True):
+def generate_password(use_numbers=True, use_lowercase=True, use_uppercase=True, use_symbols=True, use_spaces=True):
     characters = ''
     
     if use_numbers:
@@ -16,35 +18,52 @@ def generate_password(length, use_numbers=True, use_lowercase=True, use_uppercas
         characters += ' '
 
     if not characters:
-        print("Error: Please select at least one option.")
         return None
 
-    password = ''.join(random.choice(characters) for _ in range(length))
+    password = ''.join(random.choice(characters) for _ in range(8))
     return password
 
-def main():
-    print("Password Generator")
-    print("Options:")
-    print("1. Numbers")
-    print("2. Lowercase letters")
-    print("3. Uppercase letters")
-    print("4. Symbols")
-    print("5. Spaces")
-
+def generate_and_display_password():
     options = {
-        1: "use_numbers",
-        2: "use_lowercase",
-        3: "use_uppercase",
-        4: "use_symbols",
-        5: "use_spaces",
+        "use_numbers": numbers_var.get(),
+        "use_lowercase": lowercase_var.get(),
+        "use_uppercase": uppercase_var.get(),
+        "use_symbols": symbols_var.get(),
+        "use_spaces": spaces_var.get(),
     }
 
-    selected_options = [options[int(input(f"Include option {i}? (1/0): "))] for i in range(1, 6)]
+    password = generate_password(**options)
+    password_label.config(text=f"Generated Password: {password}")
 
-    password = generate_password(8, *selected_options)
+window = tk.Tk()
+window.title("Password Generator")
 
-    if password:
-        print(f"Generated Password: {password}")
+numbers_var = BooleanVar()
+lowercase_var = BooleanVar()
+uppercase_var = BooleanVar()
+symbols_var = BooleanVar()
+spaces_var = BooleanVar()
 
-if __name__ == "__main__":
-    main()
+numbers_checkbox = tk.Checkbutton(window, text="Numbers", variable=numbers_var, onvalue=True, offvalue=False)
+lowercase_checkbox = tk.Checkbutton(window, text="Lowercase", variable=lowercase_var, onvalue=True, offvalue=False)
+uppercase_checkbox = tk.Checkbutton(window, text="Uppercase", variable=uppercase_var, onvalue=True, offvalue=False)
+symbols_checkbox = tk.Checkbutton(window, text="Symbols", variable=symbols_var, onvalue=True, offvalue=False)
+spaces_checkbox = tk.Checkbutton(window, text="Spaces", variable=spaces_var, onvalue=True, offvalue=False)
+
+
+generate_button = tk.Button(window, text="Generate Password", command=generate_and_display_password)
+
+
+password_label = tk.Label(window, text="Generated Password: ")
+
+
+numbers_checkbox.grid(row=0, column=0, sticky="W")
+lowercase_checkbox.grid(row=1, column=0, sticky="W")
+uppercase_checkbox.grid(row=2, column=0, sticky="W")
+symbols_checkbox.grid(row=3, column=0, sticky="W")
+spaces_checkbox.grid(row=4, column=0, sticky="W")
+generate_button.grid(row=5, column=0, pady=10)
+password_label.grid(row=6, column=0, pady=10)
+
+
+window.mainloop()
